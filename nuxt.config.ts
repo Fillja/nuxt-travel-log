3482e09a57631cbae31f95667a7bc3f64c18057f
Robin Gombkötö <robin.gombkoto@infobric.com>
2025-07-15 15:53:33 +0200

    daisyui running

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import daisyui from "daisyui";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.css"],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {},
+      },
+      plugins: [daisyui], // ✅ correct usage
+    },
+  },
   eslint: {
     config: {
       standalone: false,
